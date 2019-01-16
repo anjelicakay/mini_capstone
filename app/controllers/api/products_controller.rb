d2dec5c79bc108115b9435e7c151facdde45f50a
anjelicakay <aktowner@gmail.com>
2019-01-16 17:39:12 -0600

    updates

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+  def first_product
+    @product = Product.first
+    render 'first_product.json.jbuilder'
+  end
+
+  def all_products
+    @products = Product.all
+    render all_products.json.jbuilder    
+  end
 end
