d4f394ba0313eb5e98a8e98db30d1caac4777ecf
anjelicakay <aktowner@gmail.com>
2019-01-16 09:49:38 -0600

    model

---
+++ db/migrate/20190116154728_create_shoes.rb
@@ -0,0 +1,12 @@
+class CreateShoes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shoes do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
