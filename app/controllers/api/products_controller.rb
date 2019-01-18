682be5669323a3ddcd99587a9b525bc8faf0e2ab
anjelicakay <aktowner@gmail.com>
2019-01-18 10:33:43 -0600

    more changes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -8,4 +8,16 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render all_products.json.jbuilder    
   end
+
+  def string_product
+    product_id = params[:id]
+    @product = Product.find(product_id)
+    render 'string_product.json.jbuilder'    
+  end
+
+    def segment_product
+    product_id = params[:id]
+    @product = Product.find(product_id)
+    render 'segment_product.json.jbuilder'    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     get '/first_product' => 'products#first_product'
     get '/all_proudcts' => 'products#all_products'
+    get '/product_by_string_params' => 'products#string_product'
+    get '/product_by_segment_params/:id' => 'products#segment_product'
   end  
 end
