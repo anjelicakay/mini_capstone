c3a830d932a18f1d86ff069f22ab74fd7acf2e02
anjelicakay <aktowner@gmail.com>
2019-01-22 12:33:58 -0600

    migrate

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Product.create!([
+  {name: "Glazed Donut", price: "1.0", image_url: "https://assets.epicurious.com/photos/54b0226d766062b20344580a/1:1/w_600%2Ch_600/51160200_glazed-doughnuts_1x1.jpg", description: "Donut glazed with sugar icing", in_stock: true},
+  {name: "Chocolate Pocket with Nutella", price: "3.0", image_url: "https://stansdonuts.com/wp-content/uploads/2018/07/Chocolate-Pocket-with-Nutella.jpg", description: "Donut dipped in chocolate icing and filled with nutella", in_stock: true},
+  {name: "Vanilla Sprinkled Glazed Donut", price: "2.0", image_url: "http://www.sunnydonuts.com/img/vanilla_frosted.png", description: "Glazed donut dipped in vanilla icing and topped with rainbow sprinkles", in_stock: true},
+  {name: "Plain Old Fashioned", price: "3.0", image_url: "https://sweets.seriouseats.com/images/20120120-oldfashioned.jpg", description: "Cake donut glazed with sugar icing", in_stock: true},
+  {name: "Chocolate Sprinkled Donut", price: "2.0", image_url: "http://www.lifeasastrawberry.com/wp-content/uploads/2016/06/baked-sprinkle-donuts-recipe.jpg", description: "Cake donut dipped in chocolate icing and topped with rainbow sprinkles", in_stock: true}
+])
